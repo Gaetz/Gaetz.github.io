f25b17149c8ea9a4a7cdea42b30acceee63a6de4
Gaetz on Emilie-Grey <gaetz@rufflerim.com>
2016-09-26 11:40:27 +0200

    Write tests for blog service

--- src/app/services/blog.service.spec.ts
+++ src/app/services/blog.service.spec.ts
@@ -4,13 +4,24 @@ import { TestBed, async, inject } from '@angular/core/testing';
 import { BlogService } from './blog.service';
 
 describe('Service: Blog', () => {
+
+  let service: BlogService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [BlogService]
     });
   });
 
+  beforeEach(inject([BlogService], (s: BlogService) => service = s));
+
   it('should ...', inject([BlogService], (service: BlogService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should list categories', () => {
+    const races = service.listCategories();
+    expect(races.length).toBe(3, 'The service should return three categories for the `listCategories()` method');
+  });
+
 });
