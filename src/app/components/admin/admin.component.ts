8c37937a3f95c22d706a80dbce5117403fa4e6ca
Gaëtan Blaise-Cazalet <gaetz@rufflerim.com>
2016-10-20 11:23:03 +0200

    Editing posts easily

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -14,16 +14,25 @@ import { Post } from '../../models/post.model';
 })
 export class AdminComponent implements OnInit {
 
+  // Db access
   categories: FirebaseListObservable<Category[]>;
   posts: FirebaseListObservable<Post[]>;
 
+  // Selected post when editing
+  selectedPost: Post = null;
+  submitLabel: String = "Ajouter";
+
+  // Keep track of categories
   private staticCategories: Array<Category> = [];
 
+  // Form controllers
   titleCtrl: FormControl;
   resumeCtrl: FormControl;
   contentCtrl: FormControl;
   authorCtrl: FormControl;
   categoryCtrl: FormControl;
+  keyCtrl: FormControl;
+  dateCtrl: FormControl;
   postForm: FormGroup;
 
   constructor(
@@ -42,12 +51,16 @@ export class AdminComponent implements OnInit {
     this.contentCtrl = this.fb.control('', Validators.required);
     this.authorCtrl = this.fb.control('Gaëtan Blaise-Cazalet', Validators.required);
     this.categoryCtrl = this.fb.control('1', Validators.required);
+    this.keyCtrl = this.fb.control('');
+    this.dateCtrl = this.fb.control(0);
     this.postForm = this.fb.group({
       title: this.titleCtrl,
       resume: this.resumeCtrl,
       content: this.contentCtrl,
       author: this.authorCtrl,
-      category: this.categoryCtrl
+      category: this.categoryCtrl,
+      key: this.keyCtrl,
+      date: this.dateCtrl
     });
     // Save categories
     this.UpdateStaticCategories();
@@ -69,22 +82,52 @@ export class AdminComponent implements OnInit {
 
   // Forms
   resetPost() {
+    // Reset post selection
+    this.selectedPost = null;
+    this.submitLabel = "Ajouter";
+    // Reset form
     this.titleCtrl.setValue('');
     this.resumeCtrl.setValue('');
     this.contentCtrl.setValue('');
     this.authorCtrl.setValue('Gaëtan Blaise-Cazalet');
     this.categoryCtrl.setValue('1');
+    this.keyCtrl.setValue('');
+    this.dateCtrl.setValue(0);
   }
 
+  // Submit post form, either for creating or editing
   submitPost(postValue, isValid: boolean) {
     if (isValid) {
-      this.addPost(
-        new Post(postValue.title, postValue.resume,
-        postValue.content, postValue.author,
-        parseInt(postValue.category, 10), this.getCategoryNameFromId(parseInt(postValue.category, 10))
-      ));
+      // Add post if not editing post
+      if(this.selectedPost == null) {
+        this.addPost(
+          new Post(postValue.title, postValue.resume,
+          postValue.content, postValue.author,
+          parseInt(postValue.category, 10), this.getCategoryNameFromId(parseInt(postValue.category, 10))
+        ));
+      // If editing post, update post
+      } else {
+        this.updatePost(
+          postValue.key, 
+          new Post(postValue.title, postValue.resume,
+            postValue.content, postValue.author,
+            parseInt(postValue.category, 10), this.getCategoryNameFromId(parseInt(postValue.category, 10)),
+            parseInt(postValue.date))
+        )
+      }
       this.resetPost();
     }
+  }
+
+  // Put data in form when a post is selected for edition
+  putPostInForm(post: Post) {
+    this.titleCtrl.setValue(post.title);
+    this.resumeCtrl.setValue(post.resume);
+    this.contentCtrl.setValue(post.content);
+    this.authorCtrl.setValue(post.author);
+    this.categoryCtrl.setValue(post.categoryId);
+    this.keyCtrl.setValue(post.$key);
+    this.dateCtrl.setValue(post.date);
   }
 
   // Retrieve a category name from the category list
@@ -103,7 +146,14 @@ export class AdminComponent implements OnInit {
     return max;
   }
 
-  // Categories
+  // Set post to be edited
+  editPost(post: Post) {
+    this.submitLabel = "Editer";
+    this.selectedPost = post;
+    this.putPostInForm(post);
+  }
+
+  // Categories services
   addCategory(name: string) {
     let maxId = this.getMaxIdCategories();
     let c = new Category(maxId, name);
@@ -111,7 +161,7 @@ export class AdminComponent implements OnInit {
     this.staticCategories.push(c);
   }
 
-  editCategory(key: string, id: number, name: string) {
+  updateCategory(key: string, id: number, name: string) {
     this.blogService.updateCategory(key, new Category(id, name));
     this.UpdateStaticCategories();
   }
@@ -121,13 +171,17 @@ export class AdminComponent implements OnInit {
     this.UpdateStaticCategories();
   }
 
-  // Posts
+  // Posts services
   goToDetails(key: string) {
       this.router.navigate(['/blog/post', key]);
   }
 
   addPost(post: Post) {
     this.blogService.addPost(post);
+  }
+
+  updatePost(key: string, post: Post) {
+    this.blogService.updatePost(key, post);
   }
 
   removePost(key: string) {
